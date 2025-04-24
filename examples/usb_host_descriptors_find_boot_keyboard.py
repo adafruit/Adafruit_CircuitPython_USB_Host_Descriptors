7782b669225851a17f0e6e664b422b819536c473
foamyguy <foamyguy@gmail.com>
2025-04-24 13:05:29 -0500

    fix references to mice

--- examples/usb_host_descriptors_find_boot_keyboard.py
+++ examples/usb_host_descriptors_find_boot_keyboard.py
@@ -7,39 +7,37 @@ import usb
 
 import adafruit_usb_host_descriptors
 
-# lists for mouse interface indexes, endpoint addresses, and USB Device instances
-# each of these will end up with length 2 once we find both mice
-mouse_interface_indexes = None
-mouse_endpoint_addresses = None
+keyboard_interface_index = None
+keyboard_endpoint_address = None
 keyboard = None
 
 # scan for connected USB devices
 for device in usb.core.find(find_all=True):
-    # check for boot mouse endpoints on this device
-    kbd_interface_index, kbd_endpoint_address = (
+    # check for boot keyboard endpoints on this device
+    keyboard_interface_index, keyboard_endpoint_address = (
         adafruit_usb_host_descriptors.find_boot_keyboard_endpoint(device)
     )
     # if a boot mouse interface index and endpoint address were found
-    if kbd_interface_index is not None and kbd_interface_index is not None:
+    if keyboard_interface_index is not None and keyboard_endpoint_address is not None:
         keyboard = device
 
         # detach device from kernel if needed
         if keyboard.is_kernel_driver_active(0):
             keyboard.detach_kernel_driver(0)
 
-        # set the mouse configuration so it can be used
+        # set the configuration so the keyboard can be used
         keyboard.set_configuration()
 
 buf = array.array("b", [0] * 8)
 
 while True:
-    # try to read data from the mouse
+    # try to read data from the keyboard
     try:
-        count = keyboard.read(mouse_endpoint_addresses, buf, timeout=10)
+        count = keyboard.read(keyboard_endpoint_address, buf, timeout=10)
 
     # if there is no data it will raise USBTimeoutError
     except usb.core.USBTimeoutError:
-        # Nothing to do if there is no data for this mouse
+        # Nothing to do if there is no data for this keyboard
         continue
 
     for b in buf:
